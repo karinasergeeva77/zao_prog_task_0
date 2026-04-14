a4675871ac62597774fb71a982f82d31770f5fda
karinasergeeva77 <karinaa.sergeeva.2002@mail.ru>
2026-04-14 18:29:58 +0300

    Create two_sum.py

---
+++ two_sum.py
@@ -0,0 +1,26 @@
+def two_sum(nums, target):
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+    return None
+
+def main():
+    try:
+        nums_input = input("Введите массив чисел через запятую: ")
+        nums = list(map(int, nums_input.split(',')))
+        target = int(input("Введите целевое значение (target): "))
+        result = two_sum(nums, target)
+        
+        if result:
+            print(f"{result}")
+        else:
+            print("Решение не найдено — нет двух чисел, дающих в сумме target")
+            
+    except ValueError:
+        print("Ошибка! Пожалуйста, вводите только целые числа.")
+    except Exception as err:
+        print(f"Произошла ошибка: {err}")
+
+if __name__ == "__main__":
+    main()
